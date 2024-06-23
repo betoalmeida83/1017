38e56d6afe246a446e228c07f9314260ace96f37
Beto Almeida <111810182+betoalmeida83@users.noreply.github.com>
2024-06-23 19:06:04 -0300

    Update Main.java
    código não havia sido commitado

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+
+        int tempoGasto = input.nextInt();
+        int velocidadeMedia = input.nextInt();
+
+        double consumo = (tempoGasto * velocidadeMedia) / 12.0;
+
+        System.out.printf("%.3f\n", consumo);
+
+        input.close();
+
     }
 }
